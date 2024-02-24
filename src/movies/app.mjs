8922497868a7555f797a304a53ab3dc4f026cdc1
Ishan Pranav <ishan.pranav@outlook.com>
2024-02-24 17:34:09 -0500

    Implement adding movies

--- src/movies/app.mjs
+++ src/movies/app.mjs
@@ -5,6 +5,7 @@
 import './config.mjs'; // first
 
 import express from 'express';
+import { access } from 'fs/promises';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { Movie } from './db.mjs';
@@ -13,10 +14,27 @@ const rootDirectory = dirname(fileURLToPath(import.meta.url));
 const bootstrapDistDirectory = '../../node_modules/bootstrap/dist/';
 const bootstrapPath = resolve(rootDirectory, bootstrapDistDirectory);
 const publicPath = resolve(rootDirectory, 'public');
+const app = express();
 
-express()
-    .use(express.static(bootstrapPath))
+let useFallbackStyles = false;
+
+try {
+    await access(bootstrapDistDirectory);
+
+    app.use(express.static(bootstrapPath));
+
+    useFallbackStyles = false;
+} catch {
+    useFallbackStyles = true;
+
+    console.log(`
+Missing optional dependency 'bootstrap'. Please run 'npm install'. Using
+fallback styles for now (requires Internet connection).`);
+}
+
+app
     .use(express.static(publicPath))
+    .use(express.urlencoded({ extended: false }))
     .set('view engine', 'hbs')
     .get('/', (_, response) => response.redirect('/movies'))
     .get('/movies', async (request, response) => {
@@ -29,8 +47,29 @@ express()
         try {
             response.render('movies', {
                 director: request.query.director,
-                foundData: await Movie.find(query)
+                foundData: await Movie.find(query),
+                useFallbackStyles: useFallbackStyles
             });
+        } catch (ex) {
+            console.log(ex);
+            response.status(500).send();
+        }
+    })
+    .get('/movies-add', (_, response) => response.render('movies-add', {
+        useFallbackStyles: useFallbackStyles,
+        year: new Date().getFullYear()
+    }))
+    .post('/movies-add', async (request, response) => {
+        const added = new Movie({
+            title: request.body.title,
+            year: request.body.year,
+            director: request.body.director
+        });
+
+        try {
+            await added.save();
+
+            response.redirect('/movies');
         } catch (ex) {
             console.log(ex);
             response.status(500).send();
